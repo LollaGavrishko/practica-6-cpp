db6c4a2646c7a792a6ab039d3ee1829fe9306643
LollaGavrishko <75338843+LollaGavrishko@users.noreply.github.com>
2021-05-09 14:07:43 +0300

    Update ideone_W5zO9v-2.cpp

--- ideone_W5zO9v-2.cpp
+++ ideone_W5zO9v-2.cpp
@@ -4,11 +4,51 @@
 //минимальный элементы этого двумерного массива. (О генерации
 //случайных чисел есть отдельная статья)
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
 using namespace std;
-
+const int a = 5;
+void func(int[][a], const int size);
+int fMin(int[][a], const int size);
+int fMax(int[][a], const int size);
 int main() {
-int k=30+rand()%31;
-cout<<k<<endl;
-
-	return 0;
+setlocale(LC_ALL, "rus");
+int M[a][a] = {};
+func(M, a); //заполняем и показываем массив
+cout « endl;
+cout « "Минимум: " « fMin(M, a) « endl;
+cout « "Максимум: " « fMax(M, a) « endl;
+return 0;
 }
+
+void func(int arr[][a], const int size) {
+srand(time(NULL));
+for (int i = 0; i < size; i++) {
+cout « "| ";
+for (int j = 0; j < size; j++) {
+arr[i][j] = 30 + rand() % 31;
+cout « arr[i][j] « " ";
+}
+cout « " |" « endl;
+}
+}
+
+int fMin(int arr[][a], const int size) {
+int min = arr[0][0];
+for (int i = 0; i < size; i++) {
+for (int j = 0; j < size; j++) {
+if (arr[i][j] < min) min = arr[i][j];
+}
+}
+return min;
+}
+
+int fMax(int arr[][a], const int size) {
+int max = arr[0][0];
+for (int i = 0; i < size; i++) {
+for (int j = 0; j < size; j++) {
+if (arr[i][j] > max) max = arr[i][j];
+}
+}
+return max;
+}	
